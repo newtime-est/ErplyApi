d8a552f6db072d0574918f0e33ec6725a53a260c
Sander Sats <sander.sats@gmail.com>
2018-02-23 15:58:36 +0200

    fix SSL version setting

--- EApi.php
+++ EApi.php
@@ -28,38 +28,38 @@ class EApi
 			Yii::log('EApi missing parameters', 'info', 'erply');
 			throw new Exception('Missing parameters', self::MISSING_PARAMETERS);
 		}
-	
+
 		//add extra params
 		$parameters['request'] = $request;
 		$parameters['clientCode'] = $this->clientCode;
 		$parameters['version'] = '1.0';
 		if($request != "verifyUser") $parameters['sessionKey'] = $this->getSessionKey();
-	
+
 		//create request
 		$handle = curl_init($this->url);
-		
+
 		//set the payload
 		curl_setopt($handle, CURLOPT_POST, true);
 		curl_setopt($handle, CURLOPT_POSTFIELDS, $parameters);
-		
+
 		//return body only
 		curl_setopt($handle, CURLOPT_HEADER, 0);
 		curl_setopt($handle, CURLOPT_RETURNTRANSFER, 1);
-		
+
 		//create errors on timeout and on response code >= 300
 		curl_setopt($handle, CURLOPT_TIMEOUT, 45);
 		curl_setopt($handle, CURLOPT_FAILONERROR, true);
 		curl_setopt($handle, CURLOPT_FOLLOWLOCATION, false);
-		
+
 		//set up host and cert verification
 		curl_setopt($handle, CURLOPT_SSL_VERIFYHOST, 2);
-		curl_setopt($handle, CURLOPT_SSLVERSION,3);
+		curl_setopt($handle, CURLOPT_SSLVERSION, CURL_SSLVERSION_DEFAULT);
 		curl_setopt($handle, CURLOPT_SSL_VERIFYPEER, false);
 		if($this->sslCACertPath) {
 			curl_setopt($handle, CURLOPT_CAINFO, $this->sslCACertPath);
 			curl_setopt($handle, CURLOPT_SSL_VERIFYPEER, true);
 		}
-				
+
 		//run
 		$response = curl_exec($handle);
 		$error = curl_error($handle);
